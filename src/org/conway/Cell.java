40b5bfb4e8390eb8b6538effde0a650c6cf023b4
John Daly <jpdaly@gmail.com>
2017-06-23 14:40:35 +0100

    next commit

--- src/org/conway/Cell.java
+++ src/org/conway/Cell.java
@@ -4,4 +4,28 @@ package org.conway;
  * Created by john on 23/06/17.
  */
 public class Cell {
+
+    boolean alive;
+
+    public Cell(boolean alive){
+        this.alive = alive;
+    }
+
+    public Cell tick(int numberOfNeighbours) {
+        Cell next = new Cell(this.alive);
+
+        if (this.alive == false) {
+            next.alive = (numberOfNeighbours == 3) ? true : false;
+        }
+
+        if (this.alive == true){
+            next.alive = (numberOfNeighbours < 2 || numberOfNeighbours > 3) ? false : true;
+        }
+
+        return next;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
 }
--- test/org/conway/CellTest.java
+++ test/org/conway/CellTest.java
@@ -2,7 +2,9 @@ package org.conway;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by john on 23/06/17.
@@ -10,10 +12,37 @@ import static org.junit.Assert.assertNotNull;
 public class CellTest {
 
     @Test
-    public void WhenCellCreatedIsValidObject(){
-        Cell cell = new Cell();
-        assertNotNull(cell);
-
+    public void DeadCell_WhenThreeNeighbours_AliveInNextGen(){
+        Cell deadCell = new Cell(false);
+        Cell nextGeneration = deadCell.tick(3);
+        assertTrue(nextGeneration.isAlive());
     }
 
+    @Test
+    public void DeadCell_WhenTwoNeighbours_DeadInNextGen(){
+        Cell deadCell = new Cell(false);
+        Cell nextGeneration = deadCell.tick(2);
+        assertFalse(nextGeneration.isAlive());
+    }
+
+    @Test
+    public void LiveCell_WhenTwoNeighbours_AliveInNextGen(){
+        Cell liveCell = new Cell(true);
+        Cell nextGeneration = liveCell.tick(2);
+        assertTrue(nextGeneration.isAlive());
+    }
+
+    @Test
+    public void LiveCell_WhenOneNeighbour_DeadInNextGen(){
+        Cell liveCell = new Cell(true);
+        Cell nextGeneration = liveCell.tick(1);
+        assertFalse(nextGeneration.isAlive());
+    }
+
+    @Test
+    public void LiveCell_WhenMoreThanThreeNeighbours_DeadInNextGen(){
+        Cell liveCell = new Cell(true);
+        Cell nextGeneration = liveCell.tick(4);
+        assertFalse(nextGeneration.isAlive());
+    }
 }
